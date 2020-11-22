342fbe362162766a24502b54d2f80ec85bacb8c8
Yeisson Venencia <yvenencia.18@est.ucab.edu.ve>
2020-11-22 01:45:27 -0400

    SIG-001: Agregada nave del jugador a la pantalla

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
 pygame.init()
 
-size = width, height = 800, 600
+window_size = window_width, window_height = 800, 600
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(window_size)
+
+# Background
+background_color = (0, 19, 26)
 
 # Title and icon
 pygame.display.set_caption('Space Invadors')
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
+
+# Player
+player_image = pygame.image.load('space-ship.png')
+player_image_with = player_image.get_width()
+player_image_height = player_image.get_height()
+
+player_position_x = (window_width - player_image_with) // 2
+player_position_y = int(window_height*0.9) - player_image_height
+
+def player():
+    screen.blit(player_image,(player_position_x,player_position_y))
 
 running = True
 while running:
@@ -16,5 +30,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill((0, 96, 128))
+    screen.fill(background_color)
+    player()
     pygame.display.update() 
