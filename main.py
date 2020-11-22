fe8c2df73c100d602d8c46d02454842ed0281a3b
Yeisson Venencia <yvenencia.18@est.ucab.edu.ve>
2020-11-22 00:43:30 -0400

    SIG-001: Cambio en el titulo de la venta y agregacion de icono de logo

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ size = width, height = 800, 600
 
 screen = pygame.display.set_mode(size)
 
+# Title and icon
+pygame.display.set_caption('Space Invadors')
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill((0, 96, 128))
+    pygame.display.update() 
