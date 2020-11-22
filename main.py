3610ee27994dd48821a9b4566a1fe8a836c89420
Yeisson Venencia <yvenencia.18@est.ucab.edu.ve>
2020-11-21 23:39:22 -0400

    SIG-001: creacion de ventana negra

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys, pygame
+pygame.init()
+
+size = width, height = 800, 600
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
